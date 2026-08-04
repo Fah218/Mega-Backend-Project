cec3411dda072ed69e5aaf0101f4d6e9a82bc1bb
Fahad Furquan <fahadsaniya666@gmail.com>
2026-08-04 23:55:57 +0530

    Initial commit: Set up models (Course, CourseProgress, OTP, etc.), backend initialization, and dependencies

---
+++ models/OTP.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const OTPSchema= new mongoose.Schema({
+
+    email:{
+        type:String,
+        required:true,
+    },
+    otp:{
+        type:String,
+        required:true,
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now,
+        expires:5*60,
+    }
+
+})
+
+
+async function sendVerification(email,otp){
+    try{
+        const mailResponse = await mailSender(email, "email verifiaction for study notion ", otp);
+
+        console.log("email sent successfully " , mailResponse);
+
+    } catch(error){
+
+    }
+}
+
+OTPSchema.pre("save", async function(next){
+    await sendVerification(this.email,this.otp);
+    next();
+});
+
+module.exports=mongoose.model("OTP",OTPSchema);
---
+++ utils/mailSender.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer")
+
+const mailSender = async (email,title,body)=>{{
+    try{
+       let transporter=nodemailer.createTransporter()
+       host:process.env.mail.Hosdt,
+       auth{
+        userprocess.env.MAIL_USER,
+        pass:process.env.Mail.Pass,
+       }
+    }
+
+
+    let infoawiattransporter .sendEmaol{
+        from:'Study notion by - fahad',
+        to='${email}',
+        subject:'${title}',
+        html:body 
+    }
+    console.log(info )
+    return info
+    catch(
+        error){
+console.error(error.message)
+    }
+}
+
+}
+
+module.exports=mailSender
